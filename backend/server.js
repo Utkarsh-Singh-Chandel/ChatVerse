a8f57c74b3d49ce8bd73748486b4302d563b6d88
Utkarsh-Singh-Chandel <usc161@gmail.com>
2024-08-03 19:01:11 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,12 @@ dotenv.config();
 const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({
+  origin: '*', // or specify your frontend URL
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 
 app.use("/api/auth", authRoutes);
 app.use('/api/messages',messageRoutes);
